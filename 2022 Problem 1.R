3db5522e328bcfb04f645119cf93b003e6c02514
whitney-lovelace <whitney.lovelace13@gmail.com>
2022-12-01 20:03:14 -0600

    AOC2022 Problem One

--- 2022 Problem 1.R
+++ 2022 Problem 1.R
@@ -2,16 +2,34 @@
 
 ## Part 1
 
+install.packages("clipr")
+library("clipr")
+
 setwd("C:/Users/whitn/OneDrive/Documents/Code/AdventOfCode2022")
-data <- read.csv("Problem_One.csv", header=TRUE,fileEncoding="UTF-8-BOM")
+#data2 <- read.table("Problem_One.csv", header=FALSE,fileEncoding="UTF-8-BOM")
 
-x <- data$Problem
 
-dif <- 2020 - x
-difInx <- dif %in% x
+data3 <- read_clip()
+data4 <- as.integer(data3)
 
-values <- difInx*x
+i <- 1
+totals <- c()
+sum <- 0
 
-values2multiply <- values[values>0]
+while (i<= length(data4)){
+  if (is.na(data4[i])){
+    totals <- append(totals, sum)
+    sum <-0
+    i <- i+1
+  } else {
+    sum <- sum + data4[i]
+    i <-i+1
+  }
+}
 
-answer <- prod(values2)
+max(totals)
+
+#Part 2
+
+top3 <- sort(totals, decreasing = TRUE)
+top3sum <- sum(top3[1:3])
